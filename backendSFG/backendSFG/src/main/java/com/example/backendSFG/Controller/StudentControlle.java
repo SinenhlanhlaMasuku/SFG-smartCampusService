4fb6f45b03d561f0dc5bfad1f9b5ecfbcc5236e4
LemogangKMoloko <Lemogangk81@gmail.com>
2025-05-15 14:22:37 +0200

    Student

---
+++ backendSFG/backendSFG/src/main/java/com/example/backendSFG/Controller/StudentControlle.java
@@ -0,0 +1,32 @@
+package com.example.backendSFG.Controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.backendSFG.Entities.StudentEntity;
+import com.example.backendSFG.Services.StudentService;
+
+public class StudentControlle {
+ @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/register")
+    public ResponseEntity<StudentEntity> registerStudent(@RequestBody Map<String, String> registrationData) {
+        try{
+            String name = registrationData.get("name");
+            String surname = registrationData.get("nurname");
+            String email = registrationData.get("email");
+            String password = registrationData.get("password");
+             StudentEntity registeredStudent = studentService.registerStudent(name, surname, email, password);
+             return new ResponseEntity<>(registeredStudent, HttpStatus.CREATED);
+        }
+        catch(IllegalArgumentException e){
+             return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+}
+}
--- backendSFG/backendSFG/src/main/java/com/example/backendSFG/Entities/StudentEntity.java
+++ backendSFG/backendSFG/src/main/java/com/example/backendSFG/Entities/StudentEntity.java
@@ -17,14 +17,55 @@ public class StudentEntity {
     private long StudentId;
 
     @Column(name = "Name")
-    private String Name;
+    private String name;
 
     @Column(name = "Surname")
-    private String Surname;
+    private String surname;
 
     @Column(name = "Email")
     private String email;
 
     @Column(name = "Password")
     private String password;
+
+     // Constructor to match the parameters used in StudentService.
+     public StudentEntity(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and setters (if needed).
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
